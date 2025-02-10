c8bbf9310bd28b2e510ed38a7c1b342b5b2ebe80
Neel Patel <136300996+patelneel9080@users.noreply.github.com>
2025-02-10 22:42:48 +0530

    Take numbers as input from the user until they enter '0'.

---
+++ infiniteLoop.py
@@ -0,0 +1,10 @@
+# Write a program using a 'while loop to:
+# - Take numbers as input from the user until they enter '0'.
+
+
+
+while True:
+    a = int(input("Enter number : "))
+    
+    if a == 0:
+        break

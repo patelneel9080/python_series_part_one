4bddffe4090bf0968b6705144abbea9e6d2e7fd2
Neel Patel <136300996+patelneel9080@users.noreply.github.com>
2025-02-10 22:56:02 +0530

    'range() function with three arguments (start, stop, step) to print multiples of 5 from 5 to 50.

---
+++ range_5_multiply.py
@@ -0,0 +1,6 @@
+# Implement a program that:
+# Uses the 'range() function with three arguments (start, stop, step) to print multiples
+# of 5 from 5 to 50.
+
+for i in range(5,51,5):
+        print(i)

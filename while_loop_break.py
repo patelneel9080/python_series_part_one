e953ba9687fb390be1ca80b2ba7998a8f346b63c
Neel Patel <136300996+patelneel9080@users.noreply.github.com>
2025-02-11 22:14:14 +0530

    Added a program to print numbers from 1 to 10, stopping at 7 using the break statement.

---
+++ while_loop_break.py
@@ -0,0 +1,11 @@
+# Implement a program using a while loop to:
+# Print numbers from 1 to 10.
+# Stop the loop using the break statement when the number is 7.
+
+i = 1
+
+while i <= 10:
+    if(i == 7):
+        break
+    print(i)
+    i = i + 1

8a6c134ad3675955c5c62b4341062d1b0e1c2073
Neel Patel <136300996+patelneel9080@users.noreply.github.com>
2025-02-11 22:35:38 +0530

    Added a program to print multiplication tables using nested loops.

---
+++ multiplication_table.py
@@ -0,0 +1,8 @@
+# Write a Python program to print a multiplication table using nested loops for numbers
+# 1x1=1
+# 1x2=2
+
+for i in range(1,6):
+    for j in range(1,11):
+        print(i,"x",j,"=",i*j)
+    print()
